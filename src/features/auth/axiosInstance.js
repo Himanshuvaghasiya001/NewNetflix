1aae5bce19afb5711502579c40fd38dd77d51369
Himanshu <hitpatel30112004@gmail.com>
2025-11-07 14:05:25 +0530

    Added frontend domain to CORS

--- src/features/auth/axiosInstance.js
+++ src/features/auth/axiosInstance.js
@@ -1,7 +1,17 @@
+// axiosInstance.js
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://newnetflixbackend.onrender.com",
+});
+
+// Automatically attach token to every request
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
